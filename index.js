9630624d36568d4fbd6f2cb6e65d810575c4a01a
priffad <priffad@gmail.com>
2023-09-22 01:02:52 +0800

    new

--- index.js
+++ index.js
@@ -77,3 +77,25 @@ const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
+// require('dotenv').config();
+// const express = require('express');
+// const mongoose = require('mongoose');
+
+// const app = express();
+
+// // Koneksi ke MongoDB
+// mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+//     .then(() => console.log('Connected to MongoDB'))
+//     .catch(error => console.error('Could not connect to MongoDB', error));
+
+// app.use(express.json());
+
+// // Routes
+// app.use('/api/auth', require('./routes/authRoutes'));
+// app.use('/api/categories', require('./routes/categoryRoutes'));
+// app.use('/api/places', require('./routes/placeRoutes'));
+
+// const PORT = 3000;
+// app.listen(PORT, () => {
+//     console.log(`Server started on http://localhost:${PORT}`);
+// });
