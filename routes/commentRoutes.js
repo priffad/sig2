74c63dac8610ace9592e2d7b5b407295c900011a
priffad <priffad@gmail.com>
2023-09-25 02:53:59 +0800

    new

--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const Comment = require('../models/Comment');
 const Place = require('../models/Place');
-
+const authMiddleware = require('../middleware/authMiddleware');
 const router = express.Router();
 
 // Get comments for a specific place
@@ -11,7 +11,7 @@ router.get('/:placeId', async (req, res) => {
 });
 
 // Create a new comment
-router.post('/', async (req, res) => {
+router.post('/',authMiddleware, async (req, res) => {
     const { name, content, placeId } = req.body;
 
     const place = await Place.findById(placeId);
