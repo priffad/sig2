acefaf8171c37664faebd7c58b55136a35cfa998
priffad <priffad@gmail.com>
2023-10-17 17:35:39 +0800

    first commit

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const Review = require('../models/Review');
 const Place = require('../models/Place');
+const User = require('../models/User');
 const { userAuthenticate } = require('../middleware/auth');
 
 const router = express.Router();
@@ -17,17 +18,25 @@ router.get('/place/:placeId', async (req, res) => {
 
 
 router.post('/place/:placeId', userAuthenticate, async (req, res) => {
-    const review = new Review({
-        ...req.body,
-        place: req.params.placeId
-    });    
     try {
+        const user = await User.findById(req.user._id);
+        if (!user) {
+            return res.status(404).send('User not found');
+        }
+
+        const review = new Review({
+            ...req.body,
+            place: req.params.placeId,
+            name: user.username
+        });
+
         await review.save();
         res.status(201).send(review);
     } catch (error) {
         res.status(400).send(error);
     }
 });
+
 
 
 
