9f3ce1f6e8af94e94f44d9c3d81ce0f5960817b9
priffad <priffad@gmail.com>
2023-12-27 01:13:45 +0800

    first commit

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors({
   origin: true,
-  methods: ["GET", "POST", "PUT", "DELETE"],
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   credentials: true,
 }));
 
