471e47d7608a0f2ddcc95d6e03ddac2ee1bc2656
priffad <priffad@gmail.com>
2024-01-18 21:57:02 +0800

    first commit

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -25,22 +25,24 @@ router.post('/register', async (req, res) => {
 
 // Login
 router.post('/login', async (req, res) => {
-  try {
-    const { username, password } = req.body;
-    const user = await User.findOne({ username });
-    if (!user) {
-        return res.status(400).send({ error: 'Invalid login credentials' });
+    try {
+      const { username, password } = req.body;
+      const user = await User.findOne({ username });
+      if (!user) {
+          return res.status(400).send({ error: 'Invalid login credentials' });
+      }
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+          return res.status(400).send({ error: 'Invalid login credentials' });
+      }
+      const token = jwt.sign({ _id: user._id }, process.env.SECRET_KEY);
+  
+    
+      res.send({ token: token, userId: user._id }); // Mengembalikan token dan userId
+    } catch (error) {
+      res.status(500).send(error);
     }
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) {
-        return res.status(400).send({ error: 'Invalid login credentials' });
-    }
-    const token = jwt.sign({ _id: user._id }, process.env.SECRET_KEY);
-    res.send( token );
-} catch (error) {
-    res.status(500).send(error);
-}
-});
+  });
 
 router.patch('/changepassword',userAuthenticate,  async (req, res) => {
     try {
