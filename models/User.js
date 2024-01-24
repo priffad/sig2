8771b04db90aac8380b39803541d5159ef72408d
priffad <priffad@gmail.com>
2024-01-25 06:48:32 +0800

    first commit

--- models/User.js
+++ models/User.js
@@ -21,6 +21,9 @@ const userSchema = new mongoose.Schema({
 });
 
 userSchema.pre('save', async function(next) {
+    if (!this.isModified('password')) {
+        return next();
+    }
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
     next();
