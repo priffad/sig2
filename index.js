ca87d4aa6a83ead734c45361784f17121e3b70f9
priffad <priffad@gmail.com>
2024-02-19 12:12:00 +0800

    first commit

--- index.js
+++ index.js
@@ -22,7 +22,6 @@ mongoose.connect(process.env.MONGO_URI, {
   useUnifiedTopology: true,
 });
 
-// Once connected to the DB, initialize our gfs instance
 let gfs;
 const conn = mongoose.connection;
 conn.once('open', () => {
--- routes/ArticleController.js
+++ routes/ArticleController.js
@@ -1,96 +1,10 @@
-// const express = require('express');
-// const multer = require('multer');
-// const { userAuthenticate } = require('../middleware/auth');
-// const Article = require('../models/Article');
-// const { getCloudinaryStorage, cloudinary } = require('../cloudinaryConfig');
 
-// // Setup multer untuk menggunakan cloudinary storage
-// const storage = getCloudinaryStorage('articles');
-// const upload = multer({ storage });
-
-// const router = express.Router();
-
-// // Membuat artikel baru
-// router.post('/', userAuthenticate, upload.single('image'), async (req, res) => {
-//     try {
-//         const article = new Article({
-//             ...req.body,
-//             imageUrl: req.file ? req.file.path : ''
-//         });
-//         await article.save();
-//         res.status(201).json(article);
-//     } catch (error) {
-//         console.error(error);
-//         res.status(500).json({ message: "Internal server error", error: error.toString() });
-//     }
-// });
-
-// // Mendapatkan semua artikel
-// router.get('/', async (req, res) => {
-//     try {
-//         const articles = await Article.find({});
-//         res.status(200).json(articles);
-//     } catch (error) {
-//         console.error(error);
-//         res.status(500).json({ message: "Error fetching articles", error: error.toString() });
-//     }
-// });
-
-// // Mendapatkan satu artikel berdasarkan id
-// router.get('/:id', async (req, res) => {
-//     try {
-//         const article = await Article.findById(req.params.id);
-//         if (!article) {
-//             return res.status(404).json({ message: 'Article not found' });
-//         }
-//         res.status(200).json(article);
-//     } catch (error) {
-//         console.error(error);
-//         res.status(500).json({ message: "Error fetching article", error: error.toString() });
-//     }
-// });
-
-// // Memperbarui artikel
-// router.patch('/:id', userAuthenticate, upload.single('image'), async (req, res) => {
-//     const { id } = req.params;
-
-//     const articleUpdates = req.body;
-//     if (req.file) {
-//         articleUpdates.imageUrl = req.file.path;
-//     }
-
-//     try {
-//         const updatedArticle = await Article.findByIdAndUpdate(id, articleUpdates, { new: true });
-//         res.status(200).json(updatedArticle);
-//     } catch (error) {
-//         console.error(error);
-//         res.status(500).json({ message: "Error updating article", error: error.toString() });
-//     }
-// });
-
-
-// // Menghapus artikel
-// router.delete('/:id', userAuthenticate, async (req, res) => {
-//     try {
-//         const article = await Article.findByIdAndRemove(req.params.id);
-//         if (article && article.imageUrl) {
-//             const publicId = article.imageUrl.split('/').pop().split('.')[0];
-//             await cloudinary.uploader.destroy(publicId);
-//         }
-//         res.status(200).json({ message: 'Article deleted successfully' });
-//     } catch (error) {
-//         console.error(error);
-//         res.status(500).json({ message: "Error deleting article", error: error.toString() });
-//     }
-// });
-
-// module.exports = router;
 const express = require('express');
 const multer = require('multer');
 const Article = require('../models/Article');
 const { userAuthenticate } = require('../middleware/auth');
 const { getCloudinaryStorage } = require('../cloudinaryConfig');
-
+const sanitizeHtml = require('sanitize-html');
 const router = express.Router();
 const storage = getCloudinaryStorage('articles');
 const upload = multer({ storage });
@@ -98,8 +12,17 @@ const upload = multer({ storage });
 // Membuat artikel baru
 router.post('/', userAuthenticate, upload.single('image'), async (req, res) => {
     try {
+        const cleanContent = sanitizeHtml(req.body.content, {
+            allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img', 'h1', 'h2', 'h3', 'p', 'br', 'ul', 'ol', 'li', 'strong', 'em', 'blockquote', 'a']),
+            allowedAttributes: {
+                'a': ['href', 'name', 'target'],
+                'img': ['src']
+            },
+        });
+
         const article = new Article({
-            ...req.body,
+            title: req.body.title,
+            content: cleanContent,
             imageUrl: req.file ? req.file.path : '',
         });
         await article.save();
@@ -134,17 +57,30 @@ router.get('/:id', async (req, res) => {
 
 // Update artikel
 router.patch('/:id', userAuthenticate, upload.single('image'), async (req, res) => {
-    const updates = req.body;
-    if (req.file) {
-        updates.imageUrl = req.file.path;
-    }
     try {
+        const cleanContent = sanitizeHtml(req.body.content, {
+            allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img', 'h1', 'h2', 'h3', 'p', 'br', 'ul', 'ol', 'li', 'strong', 'em', 'blockquote', 'a']),
+            allowedAttributes: {
+                'a': ['href', 'name', 'target'],
+                'img': ['src']
+            },
+        });
+
+        const updates = {
+            ...req.body,
+            content: cleanContent
+        };
+        if (req.file) {
+            updates.imageUrl = req.file.path;
+        }
+
         const article = await Article.findByIdAndUpdate(req.params.id, updates, { new: true });
         res.status(200).json(article);
     } catch (error) {
         res.status(500).json({ message: "Error updating article", error: error.toString() });
     }
 });
+
 
 // Delete artikel
 router.delete('/:id', userAuthenticate, async (req, res) => {
